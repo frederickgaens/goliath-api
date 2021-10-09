537dafe54364fbb9a19bfebbb7c27de70430b08f
Frederick <frederick@gaens.org>
2021-10-09 21:52:06 +0200

    project setup

---
+++ src/test/java/be/codesolutions/goliathapi/GoliathApiApplicationTests.java
@@ -0,0 +1,13 @@
+package be.codesolutions.goliathapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoliathApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
